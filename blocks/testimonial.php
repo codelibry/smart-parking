6c5b8b9a7ab8422a254b394ead3113775f11f356
tyshchukRoman <roman.t@codelibry.dev>
2025-03-27 12:11:59 +0200

    block(testimonial)

---
+++ blocks/testimonial.php
@@ -0,0 +1,29 @@
+<?php
+
+$image = get_field('testimonial__image');
+$quote = get_field('testimonial__quote');
+$position = get_field('testimonial__position');
+$company = get_field('testimonial__company');
+
+?>
+
+<div class="pt-50 pb-100 testimonial-block">
+    <div class="grid-container">
+        <div class="grid-x grid-padding-x">
+            <div class="xxlarge-12 cell flex align-center justify-center">
+                <?php if ($image) : ?>
+                    <img <?php acf_image_attrs($image) ?>>
+                <?php endif; ?>
+
+                <div class="testimonial">
+                    <div class="testimonial-wrap">
+                        <?php if ($quote) : ?>
+                            <?php echo esc_html($quote) ?>
+                        <?php endif; ?>
+                    </div>
+                    <p><?php echo esc_html($position); ?> | <strong><?php echo esc_html($company); ?></strong></p>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
--- front-page.php
+++ front-page.php
@@ -6,6 +6,7 @@ get_template_part('blocks/home-hero');
 get_template_part('blocks/industries');
 get_template_part('blocks/city-scape');
 get_template_part('blocks/left-content');
+get_template_part('blocks/testimonial');
 
 ?>
 
