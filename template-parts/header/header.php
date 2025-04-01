e547b3ca528dafe9655988da4163b7a283a17071
tyshchukRoman <roman.t@codelibry.dev>
2025-04-01 10:13:03 +0300

    feat(header): integrate with acf option

--- template-parts/header/header.php
+++ template-parts/header/header.php
@@ -1,8 +1,19 @@
+<?php
+
+$site_logo = get_field('site-logo', 'option');
+$cta_button = get_field('cta-button', 'option');
+
+?>
+
 <header>
     <div class="grid-container pt-30 pb-30">
         <div class="grid-x grid-margin-x">
             <div class="large-12 cell flex align-center space-between relative">
-                <a class="logo" href="/uk"><img src="/media/2uhb0ibr/logo.svg" alt="Smart Parking"></a>
+                <?php if($site_logo): ?>
+                    <a class="logo" href="<?php echo home_url() ?>">
+                        <img <?php acf_image_attrs($site_logo) ?>>
+                    </a>
+                <?php endif; ?>
 
                 <?php wp_nav_menu([
                     'theme_location' => 'header-menu',
@@ -20,19 +31,21 @@
                             <option value="dk">Denmark</option>
                         </select>
                     </div>
-                    <a class="mobile-menu-trigger" data-open="exampleModal1" aria-controls="exampleModal1" aria-haspopup="dialog" tabindex="0"></a>
-                    <a href="/uk/pay-a-parking-charge" class="button">Pay / Appeal Parking Charge</a>
-      <div class="region" id="cultures-region">
-                        <select id="culture-dropdown">
-                            <option value="uk" selected="">UK</option>
-                            <option value="de">Germany</option>
-                            <option value="au">Australia</option>
-                            <option value="nz">New Zealand</option>
-                            <option value="dk">Denmark</option>
-                        </select>
-                    <div class="select-selected"><img src="/img/flags/flag-uk.png" alt="UK">UK</div><div class="select-items select-hide"><div><img src="/img/flags/flag-uk.png" alt="UK">UK</div><div><img src="/img/flags/flag-germany.png" alt="Germany">Germany</div><div><img src="/img/flags/flag-australia.png" alt="Australia">Australia</div><div><img src="/img/flags/flag-newzealand.png" alt="New Zealand">New Zealand</div><div><img src="/img/flags/flag-denmark.png" alt="Denmark">Denmark</div></div></div>
-                </div>
 
+                    <a 
+                      class="mobile-menu-trigger" 
+                      data-open="exampleModal1" 
+                      aria-controls="exampleModal1" 
+                      aria-haspopup="dialog" 
+                      tabindex="0">
+                    </a>
+
+                    <?php if($cta_button): ?>
+                        <a class="button" <?php acf_link_attrs($cta_button) ?>>
+                            <?php echo $cta_button['title'] ?>
+                        </a>
+                    <?php endif; ?>
+                </div>
             </div>
         </div>
     </div>
