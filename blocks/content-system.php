5d34eb14277888b2ce9d832c6147e599b321a89b
tyshchukRoman <roman.t@codelibry.dev>
2025-04-02 11:45:58 +0300

    feat(content-system): add block

--- blocks/content-system.php
+++ blocks/content-system.php
@@ -1,51 +1,94 @@
 <?php
 
-$image = get_field('content-system__image');
-$title = get_field('content-system__title');
-$description = get_field('content-system__description');
-$systems = get_field('content-system__systems');
-
-if(!$systems || empty($systems)) {
-  return;
-}
+$image = get_array_value($args, 'image', get_field('content-system__image'));
+$title = get_array_value($args, 'title', get_field('content-system__title'));
+$description = get_array_value($args, 'description', get_field('content-system__description'));
+$systems = get_array_value($args, 'systems', get_field('content-system__systems'));
+$is_content_left = get_array_value($args, 'is_content_left', get_field('content-system__content-left'));
 
 ?>
 
-<div class="pt-100 pb-100 light-blue system-block-left">
-    <?php if($image): ?>
-        <img class="graphic" <?php acf_image_attrs($image) ?>>
-    <?php endif; ?>
+<?php if($is_content_left): ?>
 
-    <div class="grid-container">
-        <div class="grid-x grid-padding-x">
-            <div class="large-5 cell">
-                <?php if($title): ?>
-                    <h2><?php echo $title ?></h2>
-                <?php endif; ?>
+    <div class="pt-100 pb-100 light-blue system-block-left">
+        <?php if($image): ?>
+            <img class="graphic" <?php acf_image_attrs($image) ?> style="max-width:840px;">
+        <?php endif; ?>
 
-                <?php if($description): ?>
-                    <p><?php echo $description ?></p>
-                <?php endif; ?>
+        <div class="grid-container">
+            <div class="grid-x grid-padding-x">
+                <div class="large-5 cell">
+                    <?php if($title): ?>
+                        <h2><?php echo $title ?></h2>
+                    <?php endif; ?>
+
+                    <?php if($description): ?>
+                        <p><?php echo $description ?></p>
+                    <?php endif; ?>
+                </div>
             </div>
-        </div>
 
-        <div class="grid-x grid-padding-x">
-            <div class="large-6 cell">
-                <div class="grid-x grid-padding-x" data-equalizer="o15oc9-equalizer" data-resize="ufz7qg-eq" data-mutate="wd0rhf-eq" data-events="resize">
-                    <?php foreach ($systems as $system): ?>
-                        <div class="xxlarge-6 large-6 medium-6 cell">
+            <div class="grid-x grid-padding-x">
+                <div class="large-6 cell">
+                    <div class="grid-x grid-padding-x" data-equalizer="o15oc9-equalizer" data-resize="ufz7qg-eq" data-mutate="wd0rhf-eq" data-events="resize">
+                        <?php foreach ($systems as $system): ?>
+                            <div class="xxlarge-6 large-6 medium-6 cell">
 
-                            <?php get_template_part('template-parts/components/system-card', null, [
-                                'link' => get_permalink($system),
-                                'image' => get_field('system__image', $system),
-                                'title' => get_the_title($system),
-                                'description' => get_field('system__description', $system),
-                            ]) ?>
+                                <?php get_template_part('template-parts/components/system-card', null, [
+                                    'link' => get_permalink($system),
+                                    'image' => get_field('system__image', $system),
+                                    'title' => get_the_title($system),
+                                    'description' => get_field('system__description', $system),
+                                ]) ?>
 
-                        </div>
-                    <?php endforeach; ?>
+                            </div>
+                        <?php endforeach; ?>
+                    </div>
                 </div>
             </div>
         </div>
     </div>
-</div>
+
+<?php else: ?>
+
+    <div class="pb-100 light-blue system-block-right">
+        <?php if($image): ?>
+            <img class="graphic" <?php acf_image_attrs($image) ?> style="max-width:840px;">
+        <?php endif; ?>
+
+        <div class="grid-container">
+            <div class="grid-x grid-padding-x">
+                <div class="large-5 large-offset-7 cell">
+                    <?php if($title): ?>
+                        <h2><?php echo $title ?></h2>
+                    <?php endif; ?>
+
+                    <?php if($description): ?>
+                        <p><?php echo $description ?></p>
+                    <?php endif; ?>
+                </div>
+            </div>
+
+            <div class="grid-x grid-padding-x">
+                <div class="large-6 large-offset-6 cell">
+                    <div class="grid-x grid-padding-x flex flex-wrap justify-right">
+                        <?php foreach ($systems as $system): ?>
+                            <div class="xxlarge-6 large-6 medium-6 cell">
+
+                                <?php get_template_part('template-parts/components/system-card', null, [
+                                    'link' => get_permalink($system),
+                                    'image' => get_field('system__image', $system),
+                                    'title' => get_the_title($system),
+                                    'description' => get_field('system__description', $system),
+                                ]) ?>
+
+                            </div>
+                        <?php endforeach; ?>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+<?php endif; ?>
+
--- template-pages/our-system.php
+++ template-pages/our-system.php
@@ -6,10 +6,23 @@
 get_header();
 
 get_template_part('blocks/breadcrumbs');
-
 get_template_part('blocks/centered-hero');
 get_template_part('blocks/full-image');
-get_template_part('blocks/content-system');
+
+if (have_rows('content-system-repeater')) :
+    while (have_rows('content-system-repeater')) : the_row();
+
+        get_template_part('blocks/content-system', null, [
+            'image' => get_sub_field('content-system__image'),
+            'title' => get_sub_field('content-system__title'),
+            'description' => get_sub_field('content-system__description'),
+            'systems' => get_sub_field('content-system__systems'),
+            'is_content_left' => get_sub_field('content-system__content-left'),
+        ]);
+
+    endwhile;
+endif;
+
 get_template_part('blocks/testimonial');
 get_template_part('blocks/systems');
 get_template_part('blocks/related-posts');
