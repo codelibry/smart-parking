436a2f56d9ae2c14a82bde2eb26080480557acbc
tyshchukRoman <roman.t@codelibry.dev>
2025-04-11 12:06:04 +0300

    feat(why-choose-us): update why-choose-us template

--- template-pages/why-choose-us.php
+++ template-pages/why-choose-us.php
@@ -7,9 +7,11 @@ get_header();
 
 get_template_part('blocks/breadcrumbs');
 get_template_part('blocks/centered-hero');
+get_template_part('blocks/full-image');
 get_template_part('blocks/content-list');
 get_template_part('blocks/testimonial');
 get_template_part('blocks/partners');
+get_template_part('blocks/contact-form');
 get_template_part('blocks/related-posts');
 
 get_footer(); 
