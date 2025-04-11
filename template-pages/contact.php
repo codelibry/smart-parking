720b271d20db2f37f1c0eede0b985b2a3b489fe3
tyshchukRoman <roman.t@codelibry.dev>
2025-04-11 12:47:19 +0300

    feat(contact): update template

--- template-pages/contact.php
+++ template-pages/contact.php
@@ -7,6 +7,7 @@ get_header();
 
 get_template_part('blocks/breadcrumbs');
 get_template_part('blocks/contact-hero');
+get_template_part('blocks/contact-hero-secondary');
 get_template_part('blocks/locations');
 get_template_part('blocks/contact-form');
 
