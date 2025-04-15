4e20496179728830f02080a27654042757b63031
tyshchukRoman <roman.t@codelibry.dev>
2025-04-15 14:29:57 +0300

    feat(charge-appeals): update acf for blocks

--- blocks/charge-appeals.php
+++ blocks/charge-appeals.php
@@ -4,6 +4,15 @@ $title = get_field('charge-appeal__title');
 $subtitle = get_field('charge-appeal__subtitle');
 $appeals = get_field('charge-appeal__appeals');
 
+$col_1_title = get_field('charge-appeal__col-1-title');
+$col_1_button_1 = get_field('charge-appeal__col-1-button-1');
+$col_1_button_2 = get_field('charge-appeal__col-1-button-2');
+
+$col_2_title = get_field('charge-appeal__col-2-title');
+$col_2_button_1 = get_field('charge-appeal__col-2-button-1');
+$col_2_button_2 = get_field('charge-appeal__col-2-button-2');
+
+
 if(!$appeals || empty($appeals)) {
   return;
 }
@@ -38,21 +47,44 @@ if(!$appeals || empty($appeals)) {
                     <div class="appeal-reason-details" id="appeal-body-<?php echo $i + 1 ?>" style="text-align: left; display: none;">
                         <?php echo $appeal['content'] ?>
 
-                        <div class="grid-x grid-padding-x align-center text-center m-t-4">
-                            <div class="cell medium-6 small-11">
-                                <p class="cta-title"><?php _e('If your Parking Charge begins with TC, TE or 00', 'spt') ?></p>
-                                <div class="cta-buttons">
-                                    <a href="https://payments.townandcityparking.co.uk/live-pgc-user/" class="button"><?php _e('Pay a Parking Charge', 'spt') ?></a>
-                                    <a href="https://payments.townandcityparking.co.uk/live-pgc-user/" class="button hollow"><?php _e('Appeal a Parking Charge', 'spt') ?></a>
+                        <div class="grid-x grid-padding-x align-center m-t-4">
+                            <?php if($col_1_title): ?>
+                                <div class="cell medium-6 small-11">
+                                    <p class="cta-title text-center"><?php echo $col_1_title ?></p>
+                                    <div class="cta-buttons">
+                                        <?php if($col_1_button_1): ?>
+                                            <a class="button" <?php acf_link_attrs($col_1_button_1) ?>>
+                                                <?php echo $col_1_button_1['title'] ?>
+                                            </a>
+                                        <?php endif; ?>
+
+                                        <?php if($col_1_button_2): ?>
+                                            <a class="button hollow" <?php acf_link_attrs($col_1_button_2) ?>>
+                                                <?php echo $col_1_button_2['title'] ?>
+                                            </a>
+                                        <?php endif; ?>
+                                    </div>
                                 </div>
-                            </div>
-                            <div class="cell medium-6 small-11">
-                                <p class="cta-title"><?php _e('If your Parking Charge begins with SP', 'spt') ?></p>
-                                <div class="cta-buttons">
-                                    <a href="https://pcnportal.smartparking.com/VEH/Live/3sc/3sc-us/" class="button"><?php _e('Pay a Parking Charge', 'spt') ?></a>
-                                    <a href="https://pcnportal.smartparking.com/VEH/Live/3sc/3sc-us/" class="button hollow"><?php _e('Appeal a Parking Charge', 'spt') ?></a>
+                            <?php endif; ?>
+
+                            <?php if($col_2_title): ?>
+                                <div class="cell medium-6 small-11">
+                                    <p class="cta-title text-center"><?php echo $col_2_title ?></p>
+                                    <div class="cta-buttons">
+                                        <?php if($col_2_button_1): ?>
+                                            <a class="button" <?php acf_link_attrs($col_2_button_1) ?>>
+                                                <?php echo $col_2_button_1['title'] ?>
+                                            </a>
+                                        <?php endif; ?>
+
+                                        <?php if($col_2_button_2): ?>
+                                            <a class="button hollow" <?php acf_link_attrs($col_2_button_2) ?>>
+                                                <?php echo $col_2_button_2['title'] ?>
+                                            </a>
+                                        <?php endif; ?>
+                                    </div>
                                 </div>
-                            </div>
+                            <?php endif; ?>
                         </div>
                     </div>
                 <?php endforeach; ?>
