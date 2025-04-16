9fe3b31fa5058ab686c805cbad2d0316c779abec
tyshchukRoman <roman.t@codelibry.dev>
2025-04-16 12:04:07 +0300

    feat(single-industry): update template

--- single-industry.php
+++ single-industry.php
@@ -12,8 +12,23 @@ get_template_part('blocks/breadcrumbs', null, [
 ]);
 
 get_template_part('blocks/single-hero');
+
 get_template_part('blocks/full-image');
-get_template_part('blocks/content-list');
+
+get_template_part('blocks/content-list', null, [
+  'image_lg' => get_field('primary-content-list_content-list__image-lg'),
+  'image_sm' => get_field('primary-content-list_content-list__image-sm'),
+  'title' => get_field('primary-content-list_content-list__title'),
+  'description' => get_field('primary-content-list_content-list__description'),
+  'subtitle' => get_field('primary-content-list_content-list__subtitle'),
+  'list' => get_field('primary-content-list_content-list__list'),
+  'moretext' => get_field('primary-content-list_content-list__moretext'),
+  'button_1' => get_field('primary-content-list_content-list__button-1'),
+  'button_2' => get_field('primary-content-list_content-list__button-2'),
+  'additional_classes' => get_field('primary-content-list_content-list__additional-classes'),
+  'is_content_left' => get_field('primary-content-list_content-list__is-content-left'),
+]);
+
 get_template_part('blocks/cta-bar');
 
 get_template_part('blocks/content-list', null, [
