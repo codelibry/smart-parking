fb72046ab54c41f5af6b4d95541f8c92a258cf3f
tyshchukRoman <roman.t@codelibry.dev>
2025-04-16 11:52:40 +0300

    feat(single-industry): update template

--- single-industry.php
+++ single-industry.php
@@ -15,6 +15,21 @@ get_template_part('blocks/single-hero');
 get_template_part('blocks/full-image');
 get_template_part('blocks/content-list');
 get_template_part('blocks/cta-bar');
+
+get_template_part('blocks/content-list', null, [
+  'image_lg' => get_field('secondary-content-list_content-list__image-lg'),
+  'image_sm' => get_field('secondary-content-list_content-list__image-sm'),
+  'title' => get_field('secondary-content-list_content-list__title'),
+  'description' => get_field('secondary-content-list_content-list__description'),
+  'subtitle' => get_field('secondary-content-list_content-list__subtitle'),
+  'list' => get_field('secondary-content-list_content-list__list'),
+  'moretext' => get_field('secondary-content-list_content-list__moretext'),
+  'button_1' => get_field('secondary-content-list_content-list__button-1'),
+  'button_2' => get_field('secondary-content-list_content-list__button-2'),
+  'additional_classes' => get_field('secondary-content-list_content-list__additional-classes'),
+  'is_content_left' => get_field('secondary-content-list_content-list__is-content-left'),
+]);
+
 get_template_part('blocks/systems');
 
 if (have_rows('testimonials-repeater')) :
