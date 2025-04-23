566d0b9f046dec224f96b2efdd830db06f64fc88
tyshchukRoman <roman.t@codelibry.dev>
2025-04-23 12:01:30 +0300

    fix(main.js): fix mega-menu

--- js/main.js
+++ js/main.js
@@ -13,13 +13,13 @@ jQuery(function($) {
      */
     $(document).on('click', function (e) {
         // Check if the click happened outside the menu item
-        if (!$(e.target).closest('#menu-primary-menu > li').length) {
-            $('#menu-primary-menu > li').removeClass('active');
+        if (!$(e.target).closest('header .menu > li').length) {
+            $('header .menu > li').removeClass('active');
         }
     });
 
     // Prevent the menu item click from bubbling up to the document
-    $('#menu-primary-menu > li').on('click', function (e) {
+    $('header .menu > li').on('click', function (e) {
         e.stopPropagation();
 
         if ($(this).hasClass('active')) {
