88d63d9ef380b57d26af84b81ab3e4e81e63f283
tyshchukRoman <roman.t@codelibry.dev>
2025-06-02 10:23:03 +0300

    feat: single-career template

--- blocks/career-hero.php
+++ blocks/career-hero.php
@@ -13,22 +13,22 @@ $post_title = urlencode(get_the_title());
         <div class="grid-x grid-padding-x hero inner-hero flex align-center">
             <div class="large-6 cell pt-80 pb-80 flex align-center justify-left medium-flex-center">
                 <div class="hero-content flex align-left justify-center direction-column medium-center medium-flex-center">
-                    <h1 class="dot"><?php echo $title ?></h1>
+                    <h1><?php echo $title ?></h1>
                     <p class="lead">
-                        <?php echo get_the_excerpt() ?>
+                      <?php echo get_the_excerpt() ?>
                     </p>
                     <div class="article-info">
                       <div class="social-share">
                         <a href="https://twitter.com/intent/tweet?url=<?php echo $post_url; ?>&text=<?php echo $post_title; ?>" target="_blank" rel="noopener noreferrer">
-                          <img <?php img_src('x.png') ?> />
+                          <img <?php img_src('x.svg') ?> />
                         </a>
 
                         <a href="https://www.linkedin.com/sharing/share-offsite/?url=<?php echo $post_url; ?>" target="_blank" rel="noopener noreferrer">
-                          <img <?php img_src('linkedin.png') ?> />
+                          <img <?php img_src('linkedin.svg') ?> />
                         </a>
 
                         <a href="https://www.facebook.com/sharer/sharer.php?u=<?php echo $post_url; ?>" target="_blank" rel="noopener noreferrer">
-                          <img <?php img_src('facebook.png') ?> />
+                          <img <?php img_src('facebook.svg') ?> />
                         </a>
                       </div>
                     </div>
