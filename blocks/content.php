f06c9c5ac56eca8aa70fbb4570e6a8c971f97b66
tyshchukRoman <roman.t@codelibry.dev>
2025-07-29 14:37:42 +0300

    add wpautop

--- blocks/content.php
+++ blocks/content.php
@@ -7,7 +7,7 @@ $content = get_array_value($args, 'content', get_the_content());
 <div class="grid-container pt-100 rte-block">
     <div class="grid-x grid-padding-x">
         <div class="large-8 large-offset-2 medium-10 medium-offset-1 cell">
-            <?php echo $content ?>
+            <?php echo wpautop($content) ?>
         </div>
     </div>
 </div>
