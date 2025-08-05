aff604cfa03ba12b1cb9abbd92ad592bbe29f6b1
tyshchukRoman <roman.t@codelibry.dev>
2025-08-05 13:53:41 +0300

    acf(contact-form): update contact-form block

--- blocks/contact-form.php
+++ blocks/contact-form.php
@@ -1,8 +1,6 @@
 <?php
 
 $title = get_array_value($args, 'title', get_field('contact-us__title'));
-$description = get_array_value($args, 'description', get_field('contact-us__description'));
-$warning = get_array_value($args, 'warning', get_field('contact-us__warning'));
 $form_id = get_array_value($args, 'form_id', get_field('contact-us__form'));
 
 if(!$form_id) {
@@ -16,16 +14,7 @@ if(!$form_id) {
         <div class="grid-x grid-margin-x">
             <div class="large-6 large-offset-3 medium-10 medium-offset-1 text-center cell">
                 <?php if($title): ?>
-                    <h2><?php echo $title ?></h2>
-                <?php endif; ?>
-
-                <?php if($description): ?>
-                    <p><?php echo $description ?></p>
-                <?php endif; ?>
-
-                <?php if($warning): ?>
-                    <p class="mt-50" style="color: red; margin: 1rem; font-size: larger;"><?php echo $warning ?></p>
-                    <p></p>
+                    <div><?php echo $title ?></div>
                 <?php endif; ?>
             </div>
         </div>
